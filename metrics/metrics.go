bd7cfbe661657e1d3843d606316e4e44ea070f88
Jon Cohen <cohenjon@google.com>
2020-01-13 19:42:05 -0500

    Add attribution to db-ip in metrics.go

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// IP Geolocation by DB-IP (https://db-ip.com)
+
 package metrics
 
 import (
