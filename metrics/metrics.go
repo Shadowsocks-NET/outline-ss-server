1ca247a9b67b08ad1ed023609ce19e07aff66439
Jon Cohen <cohenjon@google.com>
2020-01-16 16:02:03 -0500

    Respond to review comments

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// IP Geolocation by DB-IP (https://db-ip.com)
-
 package metrics
 
 import (
